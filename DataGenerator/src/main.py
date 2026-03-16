674400739d8c66f624a1eff8d1a76a3a9b73541d
NeftaliGC <aliguiacruz@outlook.com>
2026-03-16 16:09:35 -0600

    fix/: writer mas eficiente

--- DataGenerator/src/main.py
+++ DataGenerator/src/main.py
@@ -8,8 +8,5 @@ full_path = os.path.abspath(path)
 print(f"Los archivos se guardaran en: {full_path}")
 
 if __name__ == "__main__":
-    # Generar datos
-    cities = Ciudades.generar_ciudades()
-
     # Escribir datos en CSV
-    write_csv(full_path + "/cities.csv", cities, fieldnames=["internal_id:(Ciudad)", "nombre", "estado", "pais"])
+    write_csv(full_path + "/cities.csv", Ciudades.generar_ciudades(), fieldnames=["internal_id:(Ciudad)", "nombre", "estado", "pais"])
--- DataGenerator/src/writer/csv_writer.py
+++ DataGenerator/src/writer/csv_writer.py
@@ -1,10 +1,18 @@
 import csv
 
-def write_csv(path, data, fieldnames):
-
-    with open(path, "w", newline="") as f:
+def write_csv(path, data, fieldnames, batch_size=10_000):
+    with open(path, "w", newline="", encoding="utf-8", buffering=1024*1024) as f:
         writer = csv.DictWriter(f, fieldnames=fieldnames)
         writer.writeheader()
 
+        batch = []
+
         for row in data:
-            writer.writerow(row)
+            batch.append(row)
+
+            if len(batch) == batch_size:
+                writer.writerows(batch)
+                batch.clear()
+
+        if batch:
+            writer.writerows(batch)
